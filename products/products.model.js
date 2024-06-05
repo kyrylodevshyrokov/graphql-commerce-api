e6330c1b1e7d0f53451a1c84d06430805b911bc3
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-06-05 22:33:11 +0300

    added apollo-server-express
    + added mutations for graphql

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
     id: "redshoe",
     description: "Red Shoe",
     price: 42.12,
+    reviews: [],
   },
   {
     id: "bluejean",
     description: "Blue Jean",
     price: 55.55,
+    reviews: [],
   },
 ];
 
@@ -27,8 +29,37 @@ function getProductById(id) {
   });
 }
 
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    description,
+    price,
+    reviews: [],
+  };
+
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductById(id);
+
+  if (matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment,
+    };
+
+    matchedProduct.reviews.push(newProductReview);
+
+    return newProductReview;
+  }
+}
+
 module.exports = {
   getAllProducts,
   getProductsByPrice,
   getProductById,
+  addNewProduct,
+  addNewProductReview,
 };
